80ef4f675ff04060e396ef3d15cd8ec777491103
Dawon00 <da6921@naver.com>
2021-01-29 18:32:23 +0900

    주석 추가

--- Calculator/app/src/main/java/org/techtown/calculator/MemoActivity.java
+++ Calculator/app/src/main/java/org/techtown/calculator/MemoActivity.java
@@ -23,15 +23,17 @@ public class MemoActivity extends AppCompatActivity {
         buttonMemo=findViewById(R.id.button_memo);
 
         buttonMemo.setOnClickListener(view->{
-            //case1
+            //case1->제목과 본문의 입력을 그대로 memo() 함수에 넣어 실행
             memo(editTitle.getText().toString(), editContent.getText().toString());
 
-            //case2
+            //case2->제목과 본문의 입력을 새로 만든 변수에 대입후 memo() 함수에 넣어실행
             String title = editTitle.getText().toString();
             String content = editContent.getText().toString();
             memo(title, content);
 
-            //case3
+            //case3->제목과 본문의 입력을 trim() 을 통해 불필요한 입력을 제거 후 새로 만든 변수에 대입
+            //만약 두변수 값이 모두 빈값이 아니라면 memo()함수에 넣어 실행
+            //둘다 빈값이면 토스트 실행
             String titleTrim = editTitle.getText().toString().trim();
             String contentTrim = editContent.getText().toString().trim();
 
@@ -41,6 +43,7 @@ public class MemoActivity extends AppCompatActivity {
     }
 
     private void memo(String title, String content){
+        //입력받은 제목과 내용을 가공하여 toastText라는 변수에 대입후 토스트 실행
         String toastText="제목 : " +title + "\n 본문:" + content;
         Toast.makeText(getApplicationContext(),
                 toastText,
